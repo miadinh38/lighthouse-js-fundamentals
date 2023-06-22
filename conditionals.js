73b8cd6180ca75ed2768ccda19fbc6a2746abf22
Mia Dinh <miadinh@Mias-MacBook-Air.local>
2023-06-22 01:10:44 -0400

    Resolved merge conflict by incorporating both suggestions.

--- conditionals.js
+++ conditionals.js
@@ -43,3 +43,34 @@ if (temperature < -40 || temperature > 40) {
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
+
+
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+
+
+
+
+const temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
