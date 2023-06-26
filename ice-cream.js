434acd929165eb4df2e5feee303ff4d3cd0221a8
Mia Dinh <miadinh@Mias-MacBook-Air.local>
2023-06-26 00:05:45 -0400

    Ice Cream Arrays

---
+++ ice-cream.js
@@ -0,0 +1,12 @@
+
+
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+const newFlavour = "root beer";
+const newIceCreamFlavours = iceCreamFlavours.concat(newFlavour);
+
+console.log(newIceCreamFlavours);
+console.log(newIceCreamFlavours[0]);
+console.log(newIceCreamFlavours[5]);
+console.log(newIceCreamFlavours.length);
